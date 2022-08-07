3d91294fa6a7c8f822d05793f82da26146e3aff4
Deadpo_Ol <https://github.com/devoind>
2022-08-07 22:33:24 +0300

    Сделал часть 1

--- part1/practice/bad_smell_call_chain/main.py
+++
@@ -1,7 +0,0 @@
-from part1.practice.bad_smell_call_chain.solution import Person
-
-persona = Person(100000, 45)
-
-if __name__ == "__main__":
-    print(persona.get_city_population())
-    print(persona.get_person_room())
